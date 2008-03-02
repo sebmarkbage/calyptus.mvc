580f2d51108a91bf880fa88a3ac7ef3ac4fd0bd0
Sebastian <Sebastian@8f9bea60-9d31-4c44-a11a-6657d2e7921d>
2008-03-02 00:07:21 +0000

    git-svn-id: http://mvc.svn.calyptus.se@10 8f9bea60-9d31-4c44-a11a-6657d2e7921d

---
+++ Source/Calyptus.MVC/Binding/Extensions/CacheAttribute.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Reflection;
+
+namespace Calyptus.MVC.Binding
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+    public class CacheAttribute : Attribute, IExtension
+    {
+		public HttpCacheability Cacheability { get; set; }
+
+		private bool _durationSet;
+		private int _duration;
+		public int Duration { get { return _duration; } set { _durationSet = true; _duration = value; } }
+
+		public CacheAttribute()
+		{
+			Cacheability = HttpCacheability.Public;
+
+		}
+
+		public void Initialize(MemberInfo target) { }
+
+		public void TryMapping(IHttpContext context) { }
+
+		public void OnPreAction(IHttpContext context) { }
+
+		public void OnPostAction(IHttpContext context)
+		{
+			context.Response.Cache.SetCacheability(Cacheability);
+			if (_durationSet)
+				context.Response.Cache.SetExpires(DateTime.Now.AddSeconds((double)_duration));
+		}
+
+		public bool OnError(IHttpContext context, Exception error)
+		{
+			return true;
+		}
+	}
+}
---
+++ Source/Calyptus.MVC/Binding/Extensions/IExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace Calyptus.MVC.Binding
+{
+	public interface IExtension
+	{
+		void Initialize(MemberInfo target);
+		void TryMapping(IHttpContext context);
+
+		void OnPreAction(IHttpContext context, object[] parameters);
+		bool OnError(IHttpContext context, Exception error);
+		void OnPostAction(IHttpContext context, object returnValue);
+	}
+}
